510a2cc9410f2591b20b79391ab64afa4dca005e
tkhwang <tkhwang@gmail.com>
2018-07-09 23:43:58 +0900

    [+] update

--- nomadcoin/src/blockchain.js
+++ nomadcoin/src/blockchain.js
@@ -27,18 +27,33 @@ const getTimestamp = () => new Date().getTime() / 1000;
 const getBlockchain = () => blockchain;
 
 const createHash = (index, previousHash, timestamp, data) =>
-  CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString();
+  CryptoJS.SHA256(
+    index + previousHash + timestamp + JSON.stringify(data)
+  ).toString();
 
 const createNewBlock = data => {
   const previousBlock = getLastBlock();
   const newBlockIndex = previousBlock.index + 1;
   const newTimestamp = getTimestamp();
-  const newHash = createHash(newBlockIndex, previousBlock.hash, newTimestamp, data);
-  const newBlock = new Block(newBlockIndex, newHash, previousBlock.hash, newTimestamp, data);
+  const newHash = createHash(
+    newBlockIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock = new Block(
+    newBlockIndex,
+    newHash,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  addBlockToChain(newBlock);
   return newBlock;
 };
 
-const getBlocksHash = block => createHash(block.index, block.previousHash, block.timestamp, block.data);
+const getBlocksHash = block =>
+  createHash(block.index, block.previousHash, block.timestamp, block.data);
 
 const isNewBlockValid = (candidateBlock, latestBlock) => {
   if (!isNewStructureValid(candidateBlock)) {
@@ -48,7 +63,9 @@ const isNewBlockValid = (candidateBlock, latestBlock) => {
     console.log("The candidate block doesnt have a valid index");
     return false;
   } else if (latestBlock.hash !== candidateBlock.previousHash) {
-    console.log("The previousHash of the candidate block is not the hash of the latest block");
+    console.log(
+      "The previousHash of the candidate block is not the hash of the latest block"
+    );
     return false;
   } else if (getBlocksHash(candidateBlock) !== candidateBlock.hash) {
     console.log("The hash of this block is invalid");
@@ -72,7 +89,9 @@ const isChainValid = candidateChain => {
     return JSON.stringify(block) === JSON.stringify(genesisBlock);
   };
   if (!isGenesisValid(candidateChain[0])) {
-    console.log("The candidateChains's genesisBlock is not the same as our genesisBlock");
+    console.log(
+      "The candidateChains's genesisBlock is not the same as our genesisBlock"
+    );
     return false;
   }
   for (let i = 1; i < candidateChain.length; i++) {
@@ -84,7 +103,10 @@ const isChainValid = candidateChain => {
 };
 
 const replaceChain = candidateChain => {
-  if (isChainValid(candidateChain) && candidateChain.length > getBlockchain().length) {
+  if (
+    isChainValid(candidateChain) &&
+    candidateChain.length > getBlockchain().length
+  ) {
     blockchain = candidateChain;
     return true;
   } else {
--- nomadcoin/src/server.js
+++ nomadcoin/src/server.js
@@ -1,13 +1,26 @@
-const express = require("express");
-bodyParser = require("body-parser");
-morgan = require("morgan");
-Blockchain = require("./blockchain");
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  Blockchain = require("./blockchain");
 
 const { getBlockchain, createNewBlock } = Blockchain;
 
-const PORT = 3000;
+const PORT = process.env.HTTP_PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan("combined"));
-app.listen(PORT, () => console.log(`Nomadcoin server running on ${PORT}`));
+
+app.get("/blocks", (req, res) => {
+  res.send(getBlockchain());
+});
+
+app.post("/blocks", (req, res) => {
+  const {
+    body: { data }
+  } = req;
+  const newBlock = createNewBlock(data);
+  res.send(newBlock);
+});
+
+app.listen(PORT, () => console.log(`Nomadcoin Server running on ${PORT}`));
